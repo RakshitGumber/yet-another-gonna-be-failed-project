b8df99e9d135db935982601b596932097c192084
RakshitGumber <gumberrakshit@gmail.com>
2025-11-25 22:41:25 +0530

    making hero section

--- app/app/components/Hero.vue
+++ app/app/components/Hero.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Hello</div>
+  <section
+    id="hero"
+    class="w-screen px-24 py-14 flex items-center justify-between"
+  >
+    <div class="p-[26px] flex items-center gap-14">
+      <div class="relative w-[228px] h-[250px]">
+        <div id="avatar-caricature" class="absolute"></div>
+        <div id="avatar-real" class="absolute"></div>
+        <div class="absolute w-48 h-48 bg-bgsecond"></div>
+      </div>
+      <div>Hello</div>
+    </div>
+    <div>Hello</div>
+  </section>
 </template>
--- app/app/components/Navbar.vue
+++ app/app/components/Navbar.vue
@@ -9,21 +9,28 @@
     </div>
     <nav class="px-4 flex gap-6 items-center justify-end">
       <div class="flex items-center gap-3">
-        <NuxtLink to="/learnings" class="px-4 py-2 text-xl font-medium"
+        <NuxtLink
+          to="/learnings"
+          class="px-4 py-2 text-xl font-medium hover:underline underline-offset-2"
           >Learnings</NuxtLink
         >
-        <NuxtLink to="/projects" class="px-4 py-2 text-xl font-medium"
+        <NuxtLink
+          to="/projects"
+          class="px-4 py-2 text-xl font-medium hover:underline underline-offset-2"
           >Projects</NuxtLink
         >
         <div class="flex items-center">
+          <!-- TODO: hover effect, button print resume  -->
           <button class="text-2xl px-4 py-2 cursor-pointer">
             <Icon name="solar:file-broken" :ssr="true" class="flex" />
           </button>
+          <!-- TODO: hover effect, Theme Change  -->
           <button class="text-2xl px-4 py-2 cursor-pointer">
             <Icon name="solar:moon-broken" :ssr="true" class="flex" />
           </button>
         </div>
       </div>
+      <!-- TODO: animate/connect -->
       <button
         class="text-background font-semibold text-xl py-3 px-6 bg-success rounded-xl cursor-pointer"
       >
